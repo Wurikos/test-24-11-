5742104716acd684b5c049f6ee7cf5d518a2f573
Wurikos <50768680+Wurikos@users.noreply.github.com>
2020-11-24 11:30:16 +0300

    Changes

---
+++ src/pages/index.js
@@ -0,0 +1,131 @@
+import React from "react";
+import theme from "theme";
+import { Theme, Link, Text, Box } from "@quarkly/widgets";
+import { Helmet } from "react-helmet";
+import { StackItem, Stack, Section, Override } from "@quarkly/components";
+export default (() => {
+	return <Theme theme={theme}>
+		<Helmet>
+			<title>
+				Quarkly export
+			</title>
+			<meta name={"description"} content={"Web site created using quarkly.io"} />
+			<link rel={"shortcut icon"} href={"https://uploads.quarkly.io/readme/cra/favicon-32x32.ico"} type={"image/x-icon"} />
+		</Helmet>
+		<Section
+			padding="140px 0"
+			sm-padding="40px 0"
+			background="linear-gradient(0deg,rgba(0,0,0,.2) 0%,rgba(0,0,0,.2) 100%),--color-dark url(https://images.unsplash.com/photo-1486074051793-e41332bf18fc?ixlib=rb-1.2.1&auto=format&fit=crop&w=668&q=80) 50% 15%/cover"
+			color="--light"
+			font="--base"
+		>
+			<Box>
+				<Text
+					as="h1"
+					margin="0 0 96px 0"
+					text-align="center"
+					letter-spacing="50px"
+					text-transform="uppercase"
+					font="100 132px/1.2 --fontFamily-sans"
+					lg-font="200 42px/1.2 --fontFamily-sans"
+					lg-letter-spacing="20px"
+				>
+					Mary
+					<br />
+					Cornell
+				</Text>
+			</Box>
+			<Stack>
+				<StackItem
+					width="25%"
+					lg-width="50%"
+					sm-width="100%"
+					font="--lead"
+					text-transform="uppercase"
+					letter-spacing="1px"
+				>
+					<Text margin="0px">
+						About me
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						My name is Mary Cornell, born and raised in France.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						I've been working as a professional photographer and videographer for more than 12 years.
+					</Text>
+				</StackItem>
+				<StackItem width="25%" lg-width="50%" sm-width="100%">
+					<Text margin="0px">
+						CDS Documentary Essay Prize and Best Photographer of The Year 2019 by Sony World Photography Awards.
+					</Text>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Section background="--color-darkL2" padding="64px 0" sm-padding="40px 0">
+			<Stack>
+				<StackItem width="50%" lg-width="100%">
+					<Override
+						slot="StackItemContent"
+						color="--dark"
+						padding="98px 64px"
+						background="--color-light"
+						flex-direction="column"
+					/>
+					<Text
+						as="h4"
+						margin="12px 0"
+						font="--base"
+						color="--grey"
+						letter-spacing="1px"
+						text-transform="uppercase"
+					>
+						About Zanzibar
+					</Text>
+					<Text as="h2" margin="12px 0" font="--headline2" md-font="--headline3">
+						This is one of the world's greatest cultural crossroads where Africa meets the Indian Ocean.
+					</Text>
+				</StackItem>
+				<StackItem width="50%" lg-width="100%">
+					<Override slot="StackItemContent" flex-direction="column" />
+					<Stack>
+						<StackItem width="100%" lg-width="33.3%" md-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1575999080555-3f7a698dd8d9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80) center/cover" />
+						</StackItem>
+						<StackItem width="50%" lg-width="33.3%" md-width="50%" sm-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1577315734214-4b3dec92d9ad?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=934&q=80) center/cover" />
+						</StackItem>
+						<StackItem width="50%" lg-width="33.3%" md-width="50%" sm-width="100%">
+							<Override slot="StackItemContent" padding-bottom="100%" background="url(https://images.unsplash.com/photo-1575999502951-4ab25b5ca889?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1568&q=80) center/cover" />
+						</StackItem>
+					</Stack>
+				</StackItem>
+			</Stack>
+		</Section>
+		<Link
+			font={"--capture"}
+			font-size={"10px"}
+			position={"fixed"}
+			bottom={"12px"}
+			right={"12px"}
+			z-index={"4"}
+			border-radius={"4px"}
+			padding={"5px 12px 4px"}
+			background-color={"--dark"}
+			opacity={"0.6"}
+			hover-opacity={"1"}
+			color={"--light"}
+			cursor={"pointer"}
+			transition={"--opacityOut"}
+			quarkly-title={"Badge"}
+			text-decoration-line={"initial"}
+			href={"https://quarkly.io/"}
+			target={"_blank"}
+		>
+			Made on Quarkly
+		</Link>
+	</Theme>;
+});
---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fbcbf45c260fb001e5d75fc",
+				"5fbcbf45c260fb001e5d75fe"
+			]
+		},
+		"5fbcbf45c260fb001e5d75fc": {
+			"id": "5fbcbf45c260fb001e5d75fc",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fbcbf45c260fb001e5d75fe": {
+			"id": "5fbcbf45c260fb001e5d75fe",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
